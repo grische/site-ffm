6855d7df4032dbab82ba9f5b84cc8c2bc0db5266
Christian <github@grische.xyz>
2022-10-22 12:39:31 +0200

    Revert site.mk first few lines

--- site.mk
+++ site.mk
@@ -1,40 +1,30 @@
-##	gluon site.mk makefile example
-
-##	GLUON_FEATURES
-#		Specify Gluon features/packages to enable;
-#		Gluon will automatically enable a set of packages
-#		depending on the combination of features listed
+GLUON_MULTIDOMAIN=1
+GLUON_DEPRECATED=upgrade
 
 GLUON_FEATURES := \
 	autoupdater \
+	config-mode-domain-select \
+	config-mode-geo-location-osm \
+	config-mode-mesh-vpn \
 	ebtables-filter-multicast \
 	ebtables-filter-ra-dhcp \
-	ebtables-limit-arp \
+	ebtables-source-filter \
 	mesh-batman-adv-15 \
-	mesh-vpn-fastd \
 	respondd \
 	status-page \
 	web-advanced \
+	web-private-wifi \
 	web-wizard
 
-GLUON_FEATURES_standard := \
-  wireless-encryption-wpa3
-
-##	GLUON_SITE_PACKAGES
-#		Specify additional Gluon/OpenWrt packages to include here;
-#		A minus sign may be prepended to remove a packages from the
-#		selection that would be enabled by default or due to the
-#		chosen feature flags
-
-GLUON_SITE_PACKAGES := iwinfo
-
-##	DEFAULT_GLUON_RELEASE
-#		version string to use for images
-#		gluon relies on
-#			opkg compare-versions "$1" '>>' "$2"
-#		to decide if a version is newer or not.
-
-DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+GLUON_SITE_PACKAGES := \
+	ffho-ap-timer \
+	ffho-autoupdater-wifi-fallback \
+	ffho-web-ap-timer \
+	ffmuc-autoupdater-next2stable \
+	ffmuc-mesh-vpn-wireguard-vxlan \
+	ffmuc-simple-radv-filter \
+	iwinfo \
+	respondd-module-airtime
 
 # Variables set with ?= can be overwritten from the command line
 
